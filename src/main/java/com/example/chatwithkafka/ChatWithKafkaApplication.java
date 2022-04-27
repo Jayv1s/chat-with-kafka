8ffa94674c9f218ffedf98ae62111dfd454dec84
jgoncalves_cit <jgoncalves@ciandt.com>
2022-04-27 19:33:55 -0300

    chore: Created project base

---
+++ src/main/java/com/example/chatwithkafka/ChatWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatwithkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWithKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatwithkafka/ChatWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatwithkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWithKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
